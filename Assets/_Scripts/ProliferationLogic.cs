a9e8ef90ef1e4ae57b5ca1a93cea2cc08bcf5bcc
Noegnesis <104540327+Noegnesis@users.noreply.github.com>
2023-02-18 15:09:36 -0800

    Baseline Script Ideation

---
+++ Assets/_Scripts/ProliferationLogic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Cells
+{
+    public class ProliferationLogic : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public GameObject CellOriginal;
+        public List<GameObject> CellList = new List<GameObject>();
+        public bool oneHand = false;
+        public bool twoHand = false;
+        void Start()
+        {
+            GameObject CellClone = Instantiate(CellOriginal);
+            CellList.Add(CellClone);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if (
+        }
+    }
+}
+
+
