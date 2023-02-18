9ab56a85998a665d0278c481a1f650a5e9d9dc97
Noegnesis <104540327+Noegnesis@users.noreply.github.com>
2023-02-18 13:55:45 -0800

    Basic Hud
    Text on HUD changes according to TextUpdate logic

---
+++ Assets/_Scripts/TextUpdate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace HeadsUpDisplay {
+    public class TextUpdate : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private GameObject textObject;
+        private TextMeshProUGUI myTxt;
+        void Start()
+        {
+            textObject = GameObject.Find("HUD-Text");
+            textObject.SetActive(true);
+            myTxt = textObject.GetComponent<TextMeshProUGUI>();
+            string str = "";
+            str += "<b>Move your arms in a swimming motion</b>";
+            myTxt.text = str;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
+
