2b8af0312f756a5e3ad74d35f240ec39f3dd613d
Steven Le <64396816+StevenLe2012@users.noreply.github.com>
2023-02-19 01:35:20 -0800

    constant stream of objects with probability

---
+++ Assets/_Scripts/CellSpawning/CellMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class CellMovement : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField] private float swimForce = 2f;
+    [SerializeField] private float dragForce = 1f;
+
+    [Header("References")]
+    [SerializeField] private Transform trackingReference;
+    
+    private Rigidbody _rigidbody;
+    private Quaternion _rotation;
+    private float _speed = 0f;
+    private Vector3 _velocity = new Vector3(0, 0, 0);
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        // transform.Translate(Vector3.forward * _speed);
+        // Quaternion.RotateTowards(transform.rotation, _rotation, 1);
+        
+        transform.Translate (Vector3.forward * _speed, Space.World);
+        transform.Rotate (Vector3.down * -2);
+        transform.Rotate (Vector3.back * 1);
+        transform.Rotate (Vector3.left * 3);
+
+        // Vector3 worldVelocity = trackingReference.TransformDirection(velocity);
+        // _rigidbody.AddForce(_velocity * swimForce, ForceMode.Acceleration);
+
+        // slow down after cell move forward
+        // if (_rigidbody.velocity.sqrMagnitude > 0.01f)
+        // {
+        //     _rigidbody.AddForce(-_rigidbody.velocity * dragForce, ForceMode.Acceleration);
+        // }
+    }
+
+    public void SetVelocity(Vector3 vector)
+    {
+        _velocity = vector;
+    }
+    
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+    
+    public void SetRotation(Quaternion rotation)
+    {
+        _rotation = rotation;
+    }
+}
---
+++ Assets/_Scripts/CellSpawning/SpawnCells.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CellSpawning
+{
+    public class SpawnCells : MonoBehaviour
+    {
+        [SerializeField] private Transform[] spawnPoints;
+        [SerializeField] private GameObject[] cellTypes;
+        [SerializeField] private float averageTimeToSpawn = 1f;
+        [SerializeField] private Vector3 velocity;
+        [SerializeField] private float speed;
+        [SerializeField] private Quaternion rotation;
+        
+
+
+        public float HeartBeatNum;
+
+        private float normalizedHeartBeatMultiplier = 1f;
+
+        private const float NORMAL_ARDUINO_HEARTBEAT_NUM = 100;
+
+        private void Start()
+        {
+            StartCoroutine(SpawnEnemyCoroutine());
+        }
+
+        private IEnumerator SpawnEnemyCoroutine()
+        {
+            // set to while game running
+            while (true)
+            {
+                var timeBetweenNextSpawn = averageTimeToSpawn * normalizedHeartBeatMultiplier;
+                yield return new WaitForSeconds(timeBetweenNextSpawn);
+                SpawnCell();
+            }
+        }
+        
+        private void SpawnCell()
+        {
+            if (spawnPoints.Length != 0)
+            {
+                var spawnPointIndex = Random.Range(0, spawnPoints.Length);
+                var cellTypeIndex = RandomBloodTypeIndex();
+                // if cellTypeIndex out of bounds
+                if (cellTypeIndex >= cellTypes.Length) return;
+                
+                var spawnLocation = spawnPoints[spawnPointIndex];
+                var cellType = cellTypes[cellTypeIndex];
+                // cellType.transform.position = spawnLocation.transform.position;
+                var cell = Instantiate(cellType, spawnLocation.transform);
+                // cell.GetComponent<CellMovement>().SetVelocity(velocity);
+                cell.GetComponent<CellMovement>().SetSpeed(speed);
+                cell.GetComponent<CellMovement>().SetRotation(rotation);
+                cell.GetComponent<Rigidbody>().freezeRotation = false;
+                // cell.GetComponent<Rigidbody>().freezePosition = false;
+            }
+        }
+        
+        // HARD CODED PROBABILITIES FOR CELLS
+        // 1% white blood cells
+        // 1% platelets
+        // 45% red blood cells
+        // 53% plasma and such
+        private int RandomBloodTypeIndex()
+        {
+            var index = Random.Range(0, 100);
+            if (index == 0)
+            {
+                return 0;
+            }
+            else if (index == 1)
+            {
+                return 1;
+            }
+            else if (2 <= index && index <= 46)
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+            
+        }
+    }
+}
+
