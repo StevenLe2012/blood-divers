c2ab5213a75eb72b22818a9a7d0e44ec5691ec47
Noegnesis <104540327+Noegnesis@users.noreply.github.com>
2023-02-19 06:22:05 -0800

    Changes

--- Assets/_Scripts/TextUpdate.cs
+++ Assets/_Scripts/TextUpdate.cs
@@ -45,16 +45,39 @@ namespace HeadsUpDisplay {
 
         private IEnumerator TextUpdates()
         {
-            yield return new WaitForSecondsRealtime(5);
+            yield return new WaitForSecondsRealtime(8);
             string str = "";
             str += "<b>Welcome to [app name?]</b>";
             myTxt.text = str;
-            yield return new WaitForSecondsRealtime(3);
+            yield return new WaitForSecondsRealtime(5);
             str = "<b>Firstly, lets figure out navigation!</b>";
             myTxt.text = str;
-            yield return new WaitForSecondsRealtime(3);
+            yield return new WaitForSecondsRealtime(5);
             str = "<b>Hold your hands out in front of you and hold down the triggers</b>";
             myTxt.text = str;
+            yield return new WaitForSecondsRealtime(8);
+            str = "<b>Move your arms back and let go of the triggers</b>";
+            myTxt.text = str;
+            yield return new WaitForSecondsRealtime(5);
+            str = "<b>Repeat this movement to travel across the the blood vessel</b>";
+            myTxt.text = str;
+            yield return new WaitForSecondsRealtime(5);
+            str = "<b>Keep traveling until you reach a breach in the vessel wall!</b>";
+            myTxt.text = str;
+            yield return new WaitForSecondsRealtime(15);
+            str = "<b>Hopefully you've made it by now!</b>";
+            myTxt.text = str;
+            yield return new WaitForSecondsRealtime(5);
+            str = "<b>Here you should see a floating cell</b>";
+            myTxt.text = str;
+            yield return new WaitForSecondsRealtime(5);
+            str = "<b>Grab the cell with your right hand!</b>";
+            myTxt.text = str;
+            yield return new WaitForSecondsRealtime(5);
+            str = "<b>Now also grab the cell with your left hand!</b>";
+            myTxt.text = str;
+            //yield return new WaitForSecondsRealtime()
+
         }
     }
 }
