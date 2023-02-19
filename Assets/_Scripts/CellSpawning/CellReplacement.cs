57e864425ecc7fdb3af34783d4bbb2bde6555756
Felicity H <felicityh15@gmail.com>
2023-02-19 06:25:52 -0800

    snap thingy

--- Assets/_Scripts/CellSpawning/CellReplacement.cs
+++ Assets/_Scripts/CellSpawning/CellReplacement.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class CellReplacement : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject brokenCell;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cell") {
+            Destroy(collision.gameObject);
+            brokenCell.GetComponent<SkinnedMeshRenderer>().enabled = true;
+        }
+    }
 }
